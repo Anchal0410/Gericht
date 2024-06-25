e9c748a41043d3fd3e6edf636266c7fdbe38eb35
Anchal0410 <jainanchal0410@gmail.com>
2024-06-25 23:01:01 +0530

    Update Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -39,14 +39,14 @@ const Footer = () => (
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
         <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">10:00 am - 08:00 pm</p>
         <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+        <p className="p__opensans">12:00 pm - 04:00 pm</p>
       </div>
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">2024 Gericht. All Rights reserved.</p>
     </div>
   </div>
 );
